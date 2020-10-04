6f96dc5bd1dc7be0a7dd347f10b3613dd7e898aa
Hyeongho <rlaguddgh@nate.com>
2020-10-05 07:41:31 +0900

    201005

---
+++ MapleStory/Assets/Script/Enemy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+	public Transform target;
+	public Vector3 direction;
+	public float velocity;
+	public float accelaration;
+
+
+	// Update is called once per frame
+	void Update()
+	{
+		MoveToTarget();
+	}
+
+	public void MoveToTarget()
+	{
+		target = GameObject.Find("Player").transform;
+
+		direction = (target.position - transform.position).normalized;
+
+		velocity = velocity * Time.deltaTime;
+
+		float distance = Vector3.Distance(target.position, transform.position);
+
+		if (distance <= 4.0f)
+		{
+			velocity = 0.01f;
+
+			this.transform.position = new Vector3(transform.position.x + (direction.x * velocity), transform.position.y, transform.position.z);
+		}
+		// 일정거리 밖에 있을 시, 속도 초기화 
+		else
+		{
+			velocity = 0.0f;
+		}
+	}
+}
--- MapleStory/Assets/Script/Player.cs
+++ MapleStory/Assets/Script/Player.cs
@@ -6,23 +6,91 @@ public class Player : MonoBehaviour
 {
 	public float playerSpeed = 1.0f;
 
+	public int HP;
+	public int MP;
+
+	public int ATK;
+
+	private bool LeftRight;
+	private bool isRight;
+	private bool isLeft;
+
+	public bool isGrounded;
+	private float jumpCount = 2.0f;
+
+	Rigidbody rb;
+
+	Vector3 Look;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+		isGrounded = false;
+
+		LeftRight = false;
+
+		isRight = false;
+		isLeft = false;
+
+		rb = GetComponent<Rigidbody>();
+		jumpCount = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
+
 		if (Input.GetKey(KeyCode.LeftArrow))
 		{
-			transform.Translate(Vector3.left * playerSpeed * Time.deltaTime);
+
+			Look = 0 * Vector3.forward + -1 * Vector3.right;
+
+			transform.rotation = Quaternion.LookRotation(Look);
+
+			transform.Translate(Vector3.forward * playerSpeed * Time.deltaTime);
+
 		}
 
 		else if (Input.GetKey(KeyCode.RightArrow))
 		{
-			transform.Translate(-Vector3.left * playerSpeed * Time.deltaTime);
+			Look = 0 * Vector3.forward + 1 * Vector3.right;
+
+			transform.rotation = Quaternion.LookRotation(Look);
+
+			transform.Translate(Vector3.forward * playerSpeed * Time.deltaTime);
 		}
+
+		if (isGrounded)
+		{
+			if (jumpCount > 0)
+			{
+				if (Input.GetKeyDown(KeyCode.LeftAlt))
+				{
+					rb.AddForce(new Vector3(0, 3, 0) * jumpCount, ForceMode.Impulse);
+					jumpCount--;
+				}
+				
+			}
+		}
+
+		if (Input.GetKey(KeyCode.LeftControl))
+		{
+
+		}
+
+	}
+
+	private void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.tag == "Ground")
+		{
+			isGrounded = true;
+			jumpCount = 2.0f;
+		}
+	}
+
+	void InputRight()
+	{
+
 	}
 }
